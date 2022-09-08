4af5a2ea8829e53516b6a2f1122c05c190262afd
mattgaither <benjamin.gaither@shipt.com>
2022-09-08 13:58:21 -0500

    added a footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="icon-container">
+        <a href="https://github.com/mattgaither">
+          <FaGithub className="github-icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/benjamin-gaither-88732794/"
+          className="linkedin"
+        >
+          <FaLinkedin className="linkedin-icon" />
+        </a>
+        <a href="mailto:mattgaither2@gmail.com">
+          <FaEnvelope className="email-icon" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer
